dc459326bcee138a850242e2f7faaf4e5ddb176c
David Bagwell <dmbagwell@hotmail.com>
2024-01-27 22:28:16 -0600

    - Added more smui components.

--- src/renderer/scripts/mainWindow.ts
+++ src/renderer/scripts/mainWindow.ts
@@ -1,3 +1,5 @@
+import "material-icons/iconfont/material-icons.css"
+
 import MainWindow from "@components/MainWindow.svelte"
 import type { MainAPI } from "@mainAPI" // Only the type is allowed to be imported, the type keyword must be placed outside of the brackets.
 import { RendererAPI } from "@scripts/ipc/rendererAPI"
