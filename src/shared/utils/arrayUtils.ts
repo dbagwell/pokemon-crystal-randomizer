80559c3b0900641959746d05e1098aaaf07b1b79
David Bagwell <dmbagwell@hotmail.com>
2024-01-27 22:13:46 -0600

    - Update the compact array function to filter out all nullish values.

--- src/shared/utils/arrayUtils.ts
+++ src/shared/utils/arrayUtils.ts
@@ -1,5 +1,5 @@
 import { isNullish } from "@utils"
 
-export const compact = <Value>(array: (Value | undefined)[]): Value[] => {
+export const compact = <Value>(array: (Value | nullish)[]): Value[] => {
   return array.filter((value) => { return !isNullish(value) }) as Value[]
 }
