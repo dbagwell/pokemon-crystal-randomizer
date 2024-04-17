7b7bd4589360ddd751fee05dff4682cde392fe5f
David Bagwell <dmbagwell@hotmail.com>
2024-04-16 23:26:04 -0600

    - Created a more custom autocomplete component.

---
+++ src/renderer/components/Stack.svelte
@@ -0,0 +1,48 @@
+<div bind:this={container}>
+  <slot/>
+</div>
+
+<script lang="ts">
+  import { isNumber } from "@shared/utils"
+  import { onMount } from "svelte"
+  
+  const directionMap = {
+    vertical: "column",
+    horizontal: "row",
+  }
+  
+  const alignmentMap = {
+    start: "flex-start",
+    end: "flex-end",
+    center: "center",
+    fill: "stretch",
+  }
+  
+  const distributionMap = {
+    start: "flex-start",
+    end: "flex-end",
+    center: "center",
+    fill: "space-between",
+  }
+
+  export let direction: keyof typeof directionMap
+  export let alignment: keyof typeof alignmentMap
+  export let distribution: keyof typeof distributionMap
+  export let minSpacing: number = 0
+  export let padding: number | [number, number] | [number, number, number, number] = 0
+  
+  let container: HTMLDivElement
+  
+  onMount(() => {
+    container.style.display = "flex"
+    container.style.flexDirection = directionMap[direction]
+    container.style.alignItems = alignmentMap[alignment]
+    container.style.justifyContent = distributionMap[distribution]
+    container.style.gap = `${minSpacing}px`
+    container.style.flexWrap = "nowrap"
+    container.style.padding = (isNumber(padding) ? [padding] : padding).map((value) => {
+      return `${value}px`
+    }).join(" ")
+  })
+  
+</script>
