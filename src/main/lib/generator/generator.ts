f6ec88d3f0e6f04ee255fea8c443864850e04130
David Bagwell <dmbagwell@hotmail.com>
2024-09-22 17:39:26 -0600

    - Added the ability to standardize growth rates.

--- src/main/lib/generator/generator.ts
+++ src/main/lib/generator/generator.ts
@@ -894,6 +894,16 @@ export const generateROM = (data: Buffer, customSeed: string | undefined, settin
     })
   }
   
+  if (pokemonSettings.STANDARDIZE_POKEMON_GROWTH_RATES) {
+    const growthRatesSettings = pokemonSettings.STANDARDIZE_POKEMON_GROWTH_RATES
+    
+    Object.values(updatedPokemonDataMap).forEach((pokemon) => {
+      if (!growthRatesSettings.EXCLUDE.includes(pokemon.id)) {
+        pokemon.growthRate = growthRatesSettings.GROWTH_RATE
+      }
+    })
+  }
+  
   if (
     pokemonSettings.RANDOMIZE_TM_COMPATIBILITY
     || pokemonSettings.RANDOMIZE_HM_COMPATIBILITY
--- src/shared/gameData/growthRates.ts
+++ src/shared/gameData/growthRates.ts
@@ -5,17 +5,21 @@ export const growthRatesMap: IdMap<GrowthRateId, GrowthRate> = {
   FAST: {
     id: "FAST",
     numericId: 4,
+    name: "Fast",
   },
   MEDIUM_FAST: {
     id: "MEDIUM_FAST",
     numericId: 0,
+    name: "Medium-Fast",
   },
   MEDIUM_SLOW: {
     id: "MEDIUM_SLOW",
     numericId: 3,
+    name: "Medium-Slow",
   },
   SLOW: {
     id: "SLOW",
     numericId: 5,
+    name: "Slow",
   },
 }
