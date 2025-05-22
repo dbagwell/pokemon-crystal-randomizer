0562c4957c7ac4070def080dc0b07da43aac2626
David Bagwell <dmbagwell@hotmail.com>
2025-05-22 00:18:16 -0600

    - Added warp data as well as information about data about regions of the game that can only be accessed under certain conditions.

---
+++ src/shared/types/gameData/warp.ts
@@ -0,0 +1,58 @@
+import type { GameMapId } from "@shared/types/gameDataIds/gameMaps"
+import type { ItemId } from "@shared/types/gameDataIds/items"
+import type { LogicalAccessAreaId } from "@shared/types/gameDataIds/logicalAccessAreaIds"
+import type { PokemonId } from "@shared/types/gameDataIds/pokemon"
+import type { WarpId } from "@shared/types/gameDataIds/warps"
+
+export type Warp = {
+  id: WarpId
+  mapId: GameMapId
+  index: number
+  areaId: LogicalAccessAreaId
+  type: WarpType
+  polarity?: WarpPolarity
+  distinguisher?: string
+  direction: WarpDirection
+  accessRequirements?: AccessRequirement[]
+  coordinate: [number, number]
+  romOffset: [number, number]
+  linkedWarpId?: WarpId
+}
+  
+export type AccessRequirement =
+  | WarpId
+  | LogicalAccessAreaId
+  | PokemonId
+  | ItemId
+  | number
+
+export const warpTypes = [
+  "DOOR",
+  "CAVE",
+  "STAIRS",
+  "LADDER",
+  "WARP_PAD",
+  "HOLE",
+  "NONE",
+] as const
+
+export type WarpType = typeof warpTypes[number]
+
+export const warpPolarities = [
+  "IN",
+  "OUT",
+  "UP",
+  "DOWN",
+] as const
+
+export type WarpPolarity = typeof warpPolarities[number]
+
+export const warpDirections = [
+  "UP",
+  "DOWN",
+  "LEFT",
+  "RIGHT",
+  "ANY",
+] as const
+
+export type WarpDirection = typeof warpDirections[number]
