1e5217bcb6ec2b0a64b94ded40678de04ddcb92e
David Bagwell <dmbagwell@hotmail.com>
2025-11-29 13:59:05 -0600

    - Added some item rando presets.

--- src/shared/appData/presets.ts
+++ src/shared/appData/presets.ts
@@ -2,6 +2,8 @@ export const presetIds = [
   "VANILLA",
   "CLASSIC_BINGO",
   "BINGO_PLUS",
+  "CLASSIC_MAX",
+  "MAXIMUM_PLUS",
   "CUSTOM",
 ] as const
 
@@ -22,12 +24,23 @@ export const presetsMap: IdMap<PresetId, Preset> = {
     id: "CLASSIC_BINGO",
     name: "Classic Bingo",
     description: "Mimics the settings that have been classically used by the 'Cinco Bingo' community for tournaments and weekly races.\n"
-      + "Adds some basic QoL changes, Randomizes Pokémon, Moves, and 'Non-Key' items, and skips some longer portions of the plot.",
+      + "Adds some basic QoL changes, randomizes Pokémon, Moves, and 'Non-Key' items, and skips some longer portions of the plot.",
   },
   BINGO_PLUS: {
     id: "BINGO_PLUS",
     name: "Bingo+",
     description: "Same as 'Classic Bingo' but adds some more Quality of Life changes.",
+  },
+  CLASSIC_MAX: {
+    id: "CLASSIC_MAX",
+    name: "Classic Max",
+    description: "Mimics the settings that have been classically used by the Crystal Item Rando Community for co-op races.\n"
+      + "Adds lots of QoL changes, randomizes Pokémon and moves, shuffles most items, including key items and shops, together in one big pool.",
+  },
+  MAXIMUM_PLUS: {
+    id: "MAXIMUM_PLUS",
+    name: "Maximum+",
+    description: "Same as 'Classic Max' but adds some more Quality of Life changes, includes some additional items in the pool of shuffled items, and replaces some less useful items with some more useful ones.",
   },
   CUSTOM: {
     id: "CUSTOM",
