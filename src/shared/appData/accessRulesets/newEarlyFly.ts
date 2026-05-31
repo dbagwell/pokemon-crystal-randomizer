4b1ca0e9052ef6694367240e6e69f9b735c2f20a
David Bagwell <dmbagwell@hotmail.com>
2026-05-30 23:06:05 -0600

    - Added another "early fly" access modifier that allows fly to be locked behind the SS Aqua.

---
+++ src/shared/appData/accessRulesets/newEarlyFly.ts
@@ -0,0 +1,64 @@
+import type { AccessModifier } from "@shared/appData/accessRulesets"
+
+export const newEarlyFly: AccessModifier[] = [
+  {
+    LOCATIONS: [
+      "ROUTE_44",
+    ],
+    MATCHING_REQUIREMENTS: [
+      "MAHOGANY_TOWN",
+    ],
+    ADDED_REQUIREMENTS: [
+      "STORMBADGE",
+      "HM02",
+    ],
+  },
+  {
+    LOCATIONS: [
+      "TOHJO_FALLS_EAST_SURF_AREA",
+    ],
+    MATCHING_REQUIREMENTS: [
+      "TOHJO_FALLS_WEST_SURF_AREA",
+    ],
+    ADDED_REQUIREMENTS: [
+      "STORMBADGE",
+      "HM02",
+    ],
+  },
+  {
+    LOCATIONS: [
+      "POWER_PLANT",
+    ],
+    MATCHING_REQUIREMENTS: [
+      "ROUTE_10_NORTH_POWER_PLANT_AREA_POWER_PLANT_DOOR_IN",
+    ],
+    ADDED_REQUIREMENTS: [
+      "STORMBADGE",
+      "HM02",
+    ],
+  },
+  {
+    LOCATIONS: [
+      "DIGLETTS_CAVE_VERMILION_ENTRANCE_AREA",
+    ],
+    MATCHING_REQUIREMENTS: [
+      "VERMILION_CITY_SNORLAX_AREA_CAVE_IN",
+    ],
+    ADDED_REQUIREMENTS: [
+      "STORMBADGE",
+      "HM02",
+    ],
+  },
+  {
+    LOCATIONS: [
+      "VICTORY_ROAD_GATE_EAST_AREA",
+    ],
+    MATCHING_REQUIREMENTS: [
+      "VICTORY_ROAD_GATE_NORTH_AREA",
+    ],
+    ADDED_REQUIREMENTS: [
+      "STORMBADGE",
+      "HM02",
+    ],
+  },
+]
